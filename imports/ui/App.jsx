ecf1e1ea3efe52d56f0ba70c26c615e0fc715345
teamleader <teamldader@bestplatform.net>
2016-11-03 01:26:55 +0900

    Forms and events

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,28 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
-// App component - represents the whole app
+// App compo => 예제 앱 전체를 여기에 정의
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // ref="textInput" 를 찾아서 value 를 text 에 대입
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // DB instert 후 폼에 있는 값을 ''로 지위줌
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -18,16 +34,20 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input  type="text"  ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
+
         </header>
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        <ul>   {this.renderTasks()}    </ul>
       </div>
     );
   }
 }
-
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
@@ -35,6 +55,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+      tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
