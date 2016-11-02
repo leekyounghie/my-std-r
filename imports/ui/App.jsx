4bd16b752babd7d1be4b35acc874ac1911d9065a
teamleader <teamldader@bestplatform.net>
2016-11-03 01:37:24 +0900

    Temporary UI state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,22 @@ import Task from './Task.jsx';
 
 // App compo => 예제 앱 전체를 여기에 정의
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+  // 클릭할 때마다 실행 = 상태값을 반대로
+  toggleHideCompleted() {
+   this.setState({
+     hideCompleted: !this.state.hideCompleted,
+   });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -24,7 +40,13 @@ class App extends Component {
   }
 
   renderTasks() {
-    return this.props.tasks.map((task) => (
+
+    let filteredTasks = this.props.tasks;
+    // 체크박스 체크시 => 필터 적용
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -33,7 +55,17 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List:7.Temp UI state({this.props.incompleteCount})</h1>
+
+            <label className="hide-completed">
+                <input
+                  type="checkbox"
+                  readOnly
+                  checked={this.state.hideCompleted}
+                  onClick={this.toggleHideCompleted.bind(this)}
+                />
+                Hide Completed Tasks
+            </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input  type="text"  ref="textInput"
@@ -51,10 +83,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired, // 코드 추가
 };
 
 export default createContainer(() => {
   return {
       tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+      incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),  // 코드 추가
   };
 }, App);
